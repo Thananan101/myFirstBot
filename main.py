3945b6a303d1aa79d1ebf9e33b5394995b7d52d6
Thananan Pimpanon <>
2021-06-04 17:52:20 +0000

    add sticker for $ปืน able to join leave and play a video from youtube (only via URL) dont have a queue tho

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ from replit import db
 from keep_awake import keep_alive
 from discord.ext import commands
 
+from discord import FFmpegPCMAudio
+from discord.utils import get
+import youtube_dl
+
 TOKEN = ('ODQ5OTg3ODM5Mjc3NzkzMjgw.YLjK3A.AA6ZPnjXHXUe4k0x_oiYNqnqq4Y')
 
 client = commands.Bot(command_prefix = '$')
@@ -16,9 +20,10 @@ bLung = ["ไอ้สัสลุง หัดใช้สมองบ้าง
 @client.event
 async def on_ready():
   print('we have logged in as {0.user}'.format(client))
-  
 
-@client.command(pass_context = True)
+
+
+@client.command(pass_context = True, aliases=["j"])
 async def join(ctx):
   if (ctx.author.voice):
     channel = ctx.author.voice.channel
@@ -26,9 +31,9 @@ async def join(ctx):
     await ctx.send("เข้ามาแล้วขอรับนายท่าน มีอะไรให้รับใช้")
   else:
     await ctx.message.channel.send("เข้าซักห้องก่อนค่อยเรียกกู ไอ่ฟาย")
-  
 
-@client.command(pass_context = True)
+
+@client.command(pass_context = True, aliases=["l"])
 async def leave(ctx):
   if (ctx.voice_client):
     await ctx.voice_client.disconnect()
@@ -36,34 +41,52 @@ async def leave(ctx):
   else:
     await ctx.send("ไม่ กูไม่ออก ออกแล้วจะเอาอะไรแดก")
 
-@client.command(pass_context = True)
+@client.command(pass_context = True, aliases=["ปิง"])
 async def ping(ctx):
   await ctx.message.channel.send("Pong")
 
-@client.event
-async def on_message(message):
-  if message.author == client.user:
+@client.command(pass_context = True)
+async def roll(ctx):
+  await ctx.message.channel.send(ctx.author.mention + "  " + str(randrange(100)))
+
+@client.command(pass_context = True, aliases=["หยิ่ง"])
+async def arrogant(ctx):
+  await ctx.message.channel.send("จริง ในดิสมีแต่คนหยิ่ง หยิ่งเกิ๊นนนน")
+
+@client.command(pass_context = True, aliases=["เอ้ว"])
+async def elf(ctx):
+  await ctx.message.channel.send("Jett korean อะ seoldam แต่น้องเอ้วอะ so hot")
+
+# how to use custom emoji: <:emojiName:emojiID> you can get the id from sticker URL
+@client.command(pass_context = True, aliases=["ปืน"])
+async def puen(ctx):
+  await ctx.message.channel.send(bPuen[randrange(len(bPuen))] + " <:kwy:844178327925424138>  <:puenKuan:841398727641137152>")
+
+@client.command(pass_context = True, aliases=["ลุง"])
+async def lung(ctx):
+  await ctx.message.channel.send(bLung[randrange(len(bLung))] + " :footprints:")
+
+
+@client.command(pass_context = True, aliases=["p", "pl"])
+async def play(ctx, url:str):
+  channel = ctx.author.voice.channel
+  voice_client = get(client.voice_clients, guild=ctx.guild)
+
+  YDL_OPTIONS = {'format': 'bestaudio', 'noplaylist': 'True'}
+  FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
+
+  if not voice_client.is_playing():
+    with youtube_dl.YoutubeDL(YDL_OPTIONS) as ydl:
+      info = ydl.extract_info(url, download=False)
+      URL = info['formats'][0]['url']
+    voice_client.play(discord.FFmpegPCMAudio(URL, **FFMPEG_OPTIONS))
+    voice_client.is_playing()
+  else:
+    await ctx.message.channel.send("ร้องเพลงอยู่ไอ้สัส ไม่ว่างโว้ยยย")
     return
-  if message.content.startswith('$hello'):
-    await message.channel.send('hello there.')
-  
-  if message.content.startswith('$roll'):
-    await message.channel.send(message.author.mention + "  " + str(randrange(100)))
 
-  if message.content.startswith('$หยิ่ง'):
-    await message.channel.send("จริง ในดิสมีแต่คนหยิ่ง หยิ่งเกิ๊นนนน")
 
-  if message.content.startswith('$เอ้ว'):
-    await message.channel.send("Jett korean อะ seoldam แต่น้องเอ้วอะ so hot")
-  
-  if message.content.startswith('$ปืน'):
-    await message.channel.send(bPuen[randrange(len(bPuen))] + " :footprints:")
 
-  if message.content.startswith('$เท็น'):
-    await message.channel.send("เหล่ท่อ")
-  
-  if message.content.startswith('$ลุง'):
-    await message.channel.send(bLung[randrange(len(bLung))] + " :footprints:")
 
 
 
